acd8ae45cae48d44c52d7bd47e1338e8b8f5f471
vvlnko <lavrenenkoviktoren@gmail.com>
2023-03-22 16:27:16 +0200

    Fix the typos

--- contracts/interfaces/IYieldSource.sol
+++ contracts/interfaces/IYieldSource.sol
@@ -9,19 +9,19 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 */
 interface IYieldSource is IERC20 {
 
-    /// @notice Supply tokens to the yield source.
+    /// @notice Supplies tokens to the yield source.
     /// @param from Address of the entity depositing tokens to the pool.
     /// @param to Address of the user receiving the shares of the pool.
     /// @param mintAmount Amount of deposited tokens.
     function supplyTokenTo(address from, address to, uint256 mintAmount) external;
 
-    /// @notice Redeem tokens from the yield source.
+    /// @notice Redeems tokens from the yield source.
     /// @param from Address of the entity withdrawing shares from.
     /// @param to Address of the entity that will receive the deposited tokens.
     /// @param amount Amount of tokens to be redeemed.
     function redeemToken(address from, address to, uint256 amount) external;
 
-    /// @notice Withdraw accrued interest from the yieldSource.
+    /// @notice Withdraws accrued interest from the yieldSource.
     /// @param to Address of an account that will receive the interest.
     /// @return True if operation was successful.
     function withdrawInterest(address to) external returns (bool);
@@ -30,11 +30,11 @@ interface IYieldSource is IERC20 {
     /// @return The ERC20 asset token address.
     function depositToken() external view returns (address);
 
-    /// @notice Return the total number of deposited funds along with accrued interest.
+    /// @notice Returns the total number of deposited funds along with accrued interest.
     /// @return The uin256 value of totalBalance.
     function totalBalance() external view returns (uint256);
 
-    /// @notice Return the total accrued interest.
+    /// @notice Returns the total accrued interest.
     /// @return The uin256 value of total accrued interest.
     function getTotalInterest() external view returns (uint256);
 }
